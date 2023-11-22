7053dd0b51c1a584cfd0d0a9b3a8f1d85e561a5a
Mrinal Anand <mrinal.anand9123@gmail.com>
2023-11-23 05:13:27 +0530

    s

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Search from '../components/Search'
 const Homepage = ({onSearch}) => {
     return (
-      <div>
+      <div className='th'>
         <h1 className="title-heading">
           <h3>Click  On Post to see Blogs and <br />Click on each Blog to see Full Description <br /> Blog.</h3>
           <Search onSearch={onSearch}/>
