5680bbde37364f0b5d0879769e528c30b481edad
Mrinal Anand <mrinal.anand9123@gmail.com>
2023-11-23 05:09:42 +0530

    g

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ const Homepage = ({onSearch}) => {
     return (
       <div>
         <h1 className="title-heading">
+          <h3>Click  On Post to see Blogs and <br />Click on each Blog to see Full Description <br /> Blog.</h3>
           <Search onSearch={onSearch}/>
         </h1>
       </div>
